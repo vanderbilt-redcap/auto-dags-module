7f2c98d89a010775ab5ce526a05c5d4ef52f6d21
Mark McEver <mark@fastmail.net>
2022-10-26 11:33:34 -0500

    Prevented a memory issue from unnecessarily loading data

--- AutoDAGsExternalModule.php
+++ AutoDAGsExternalModule.php
@@ -30,7 +30,7 @@ class AutoDAGsExternalModule extends \ExternalModules\AbstractExternalModule{
 			$groupId = null;
 		}
 		else{
-			$fieldLabel = $this->getChoiceLabel(array('field_name' => $dagFieldName, 'value' => $fieldValue, 'record_id' => $record));
+			$fieldLabel = $this->getChoiceLabels($dagFieldName)[$fieldValue];
 
 			$groupName = $fieldLabel . self::LABEL_VALUE_SEPARATOR . $fieldValue;
 
